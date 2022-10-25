457277117e92998aafa86cfe8d40cba576b49e5f
Amit Samadder <amiamitswe@gmail.com>
2022-10-25 18:21:24 +0600

    add article useing form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,22 @@ class ArticlesController < ApplicationController
   def index
     @all_articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+
+    @article = Article.new(params.require(:article).permit(:title, :description, :count, :isActive))
+    if @article.save
+      # redirect_to article_path(@article)
+      # or
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
   def show
     # @ convert regular variable to instance variable
 
