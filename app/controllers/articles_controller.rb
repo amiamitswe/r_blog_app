fa7a6ced4568efe667fc4f76c5d4fcad3aec56c6
Amit Samadder <amiamitswe@gmail.com>
2022-10-26 13:07:53 +0600

    fix validation and flash message issue

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,15 +9,23 @@ class ArticlesController < ApplicationController
   end
 
   def create
-
     @article = Article.new(params.require(:article).permit(:title, :description, :count, :isActive))
-    if @article.save
+    if @article.valid?
+      @article.save
       # redirect_to article_path(@article)
       # or
       redirect_to @article
     else
-      render 'new'
+      # # if generate flash message then receive by flash[:messages]
+      # flash.now[:messages] = @article.errors.full_messages
+      # # if noting then can access errors using @article.errors in view .erb file
+
+      # fix to render error message pass status code
+      # render :new, status: 201
+      # or use "data: { turbo: false }," inside form_with as attribute
+      render :new
     end
+    # render nothing: true, status: 201
   end
   def show
     # @ convert regular variable to instance variable
