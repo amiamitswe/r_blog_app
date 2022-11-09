341c231d6f5a60d4a19c1a841e283ca396e54313
Amit Samadder <amiamitswe@gmail.com>
2022-11-09 19:06:19 +0600

    create new user backend and ui

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: [:show]
+
+  def show
+  end
+
   def new
     @user = User.new
+  end
+
+  def create
+    # p params[:user]
+    # # debugger
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      flash[:notice] = "#{@user.username}User create success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
   end
 end
